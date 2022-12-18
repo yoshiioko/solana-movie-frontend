958556bb4c5ee5675828e9c22cc5b6ab0dbac0a6
yoshiioko <acardoza@pdx.edu>
2022-12-17 17:50:56 -0800

    reformatt using Prettier

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,25 +1,21 @@
-import { Card } from './Card'
-import { FC, useEffect, useState } from 'react'
-import { Movie } from '../models/Movie'
+import { Card } from "./Card";
+import { FC, useEffect, useState } from "react";
+import { Movie } from "../models/Movie";
 
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_PROGRAM_ID = "CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN";
 
 export const MovieList: FC = () => {
-    const [movies, setMovies] = useState<Movie[]>([])
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    useEffect(() => {
-        setMovies(Movie.mocks)
-    }, [])
-    
-    return (
-        <div>
-            {
-                movies.map((movie, i) => {
-                    return (
-                        <Card key={i} movie={movie} />
-                    )
-                })
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    setMovies(Movie.mocks);
+  }, []);
+
+  return (
+    <div>
+      {movies.map((movie, i) => {
+        return <Card key={i} movie={movie} />;
+      })}
+    </div>
+  );
+};
