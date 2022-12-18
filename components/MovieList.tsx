7d2514e7412d3df426b3c88b052b731811ad53a3
yoshiioko <acardoza@pdx.edu>
2022-12-17 18:32:31 -0800

    deserialize program data onto array for displaying in UI.

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,14 +1,31 @@
 import { Card } from "./Card";
 import { FC, useEffect, useState } from "react";
 import { Movie } from "../models/Movie";
+import * as Web3 from "@solana/web3.js";
 
 const MOVIE_REVIEW_PROGRAM_ID = "CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN";
 
 export const MovieList: FC = () => {
+  const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
-    setMovies(Movie.mocks);
+    connection
+      .getProgramAccounts(new Web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID))
+      .then(async (accounts) => {
+        const movies: Movie[] = accounts.reduce(
+          (accum: Movie[], { pubkey, account }) => {
+            const movie = Movie.deserialize(account.data);
+            if (!movie) {
+              return accum;
+            }
+
+            return [...accum, movie];
+          },
+          []
+        );
+        setMovies(movies);
+      });
   }, []);
 
   return (
